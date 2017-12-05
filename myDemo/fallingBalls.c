8a5c38fa90d4ad821ba628e77c6234548fac1bb6
sdmunozsierra <sdmunozsierra@miners.utep.edu>
2017-12-05 11:06:16 +0000

    Two balls are working at their rail

--- myDemo/fallingBalls.c
+++ myDemo/fallingBalls.c
@@ -1,6 +1,5 @@
 #include <msp430.h>
   
-#include <msp430.h>
 #include <libTimer.h>
 #include <lcdutils.h>
 #include <lcddraw.h>
@@ -8,44 +7,86 @@
 #include <shape.h>
 #include <abCircle.h>
 
+#include <stdlib.h>
+#include <stdio.h>
+
 #define GREEN_LED BIT6
 
 //GREEN COLUMN
 
 //Frett Verde
-AbRect frettV = {abRectGetBounds, abRectCheck, {10,10}}; /**< 10x10 rectangle */
+AbRect frettV0= {abRectGetBounds, abRectCheck, {30,5}}; /**< 10x10 rectangle */
+AbRect frettV1= {abRectGetBounds, abRectCheck, {30,5}}; /**< 10x10 rectangle */
+AbRect frettV2= {abRectGetBounds, abRectCheck, {30,5}}; /**< 10x10 rectangle */
 
 //Fret Verde de abajo layer 
-Layer lFrettVerde = {		/** Layer with a red square for testing*/
-  (AbShape *)&frettV,
-  {14, screenHeight-10},        /** bottom right */
+Layer lFrettVerde0 = {		/** Layer with a red square for testing*/
+  (AbShape *)&frettV0,
+  {0, screenHeight-20},        /** bottom right */
   {0,0}, {0,0},		        /* last & next pos */
-  COLOR_GREEN,
+  COLOR_YELLOW,
   0,
 };
-
+Layer lFrettVerde1 = {		/** Layer with a red square for testing*/
+  (AbShape *)&frettV1,
+  {0, screenHeight-10},        /** bottom right */
+  {0,0}, {0,0},		        /* last & next pos */
+  COLOR_GREEN,
+  &lFrettVerde0,
+};
+Layer lFrettVerde2 = {		/** Layer with a red square for testing*/
+  (AbShape *)&frettV2,
+  {0, screenHeight},        /** bottom right */
+  {0,0}, {0,-5},		        /* last & next pos */
+  COLOR_ORANGE,
+  &lFrettVerde1,
+};
 //Nota Verde0
 Layer notaV0 = {		/** Layer with a green circle */
   (AbShape *)&circle12,
   {15, 5},              /** Top Left Corner */
   {0,0}, {0,0},	        /* last & next pos */
-  COLOR_VIOLET,
-  &lFrettVerde,
+  COLOR_GREEN,
+  &lFrettVerde2,
 };
 
-// frett bounds
-AbRectOutline frettVOutline = {
-    abRectOutlineGetBounds, abRectOutlineCheck, 
-    {10, 10}  //size
+//Frett Rojo
+AbRect frettR0= {abRectGetBounds, abRectCheck, {30,5}}; //Rectangle zone
+AbRect frettR1= {abRectGetBounds, abRectCheck, {30,5}}; //Rectangle zone
+AbRect frettR2= {abRectGetBounds, abRectCheck, {30,5}}; //Rectangle zone
+
+//Fret Rojo de abajo layer 
+Layer lFrettRojo0 = {		/** Layer with a red square for testing*/
+  (AbShape *)&frettR0,
+  {30, screenHeight-20},        /** bottom right */
+  {0,0}, {0,0},		        /* last & next pos */
+  COLOR_YELLOW,
+  0,
 };
- 
-Layer frettVfield = {		/* playing field as a layer */
-  (AbShape *) &frettVOutline,
-  {14, screenHeight-10}, //location
-  {0,0}, {0,0},		/* last & next pos */
-  COLOR_ORANGE_RED,
-  &notaV0,
+Layer lFrettRojo1 = {		/** Layer with a red square for testing*/
+  (AbShape *)&frettR1,
+  {30, screenHeight-10},        /** bottom right */
+  {0,0}, {0,0},		        /* last & next pos */
+  COLOR_GREEN,
+  &lFrettRojo0,
 };
+Layer lFrettRojo2 = {		/** Layer with a red square for testing*/
+  (AbShape *)&frettR2,
+  {30, screenHeight},        /** bottom right */
+  {0,0}, {0,-5},		        /* last & next pos */
+  COLOR_ORANGE,
+  &lFrettRojo1,
+};
+//Nota Roja0
+Layer notaR0 = {		/** Layer with a green circle */
+  (AbShape *)&circle12,
+  {50, 5},              /** Top Left Corner */
+  {0,0}, {0,0},	        /* last & next pos */
+  COLOR_RED,
+  &lFrettRojo2,
+};
+
+
 
 /** Moving Layer
  *  Linked list of layer references
@@ -57,14 +98,13 @@ typedef struct MovLayer_s {
   struct MovLayer_s *next;
 } MovLayer;
 
-//nota Verde Cayendo
-                                //Add if you use more layers
-MovLayer mlV = { &notaV0, {0,1}, 0 }; 
+/** Moving layers are independent from each other */
+MovLayer mlV = { &notaV0, {0,3}, 0 }; 
+MovLayer mlR = { &notaR0, {0,2}, 0 }; 
 
 //END GREEN COLUMN
 
-void movLayerDraw(MovLayer *movLayers, Layer *layers)
-{
+void movLayerDraw(MovLayer *movLayers, Layer *layers){
   int row, col;
   MovLayer *movLayer;
 
@@ -100,28 +140,33 @@ void movLayerDraw(MovLayer *movLayers, Layer *layers)
   } // for moving layer being updated
 }	  
 
-void mlVAdv(MovLayer *ml, Region *frett){
-//void mlVAdv(MovLayer *ml, Region *fence){
-  Vec2 newPos;
-  u_char axis;
-  Region shapeBoundary;
-  //Region frettBoundary;
-
-  //layerGetBounds(&fieldLayer, &fieldFence);
-
-  for (; ml; ml = ml->next) {
-    vec2Add(&newPos, &ml->layer->posNext, &ml->velocity);
-    abShapeGetBounds(ml->layer->abShape, &newPos, &shapeBoundary);
-    int velocity = ml->velocity.axes[axis] = -ml->velocity.axes[axis];
-    ml->layer->posNext = newPos;
-  } /** for ml */
+/** return a Vector on position */
+Vec2 selectPosition(char p){
+    Vec2 newPos;
+    if (p == 0){
+        newPos.axes[0] = 15;
+        newPos.axes[1] = 5;
+    }
+    if (p == 1){
+        newPos.axes[0] = 50;
+        newPos.axes[1] = 5;
+    }
+    if (p == 2){
+        newPos.axes[0] = 80;
+        newPos.axes[1] = 5;
+    }
+    if (p == 3){
+        newPos.axes[0] = 110;
+        newPos.axes[1] = 5;
+    }
+    return newPos;
 }
 
+/** Glonal Variables */
 u_int bgColor = COLOR_WHITE;     /**< The background color */
 int redrawScreen = 1;           /**< Boolean for whether screen needs to be redrawn */
 
-Region fieldFence;		/**< fence around playing field  */
-
+char* score; 
 
 /** Initializes everything, enables interrupts and green LED, 
  *  and handles the rendering for the screen
@@ -132,36 +177,93 @@ void main()
   P1OUT |= GREEN_LED;
   configureClocks();
   lcd_init();
-  shapeInit();
+  p2sw_init(15);
 
-  p2sw_init(1);
+  layerInit(&notaV0 );
+  layerDraw(&notaV0 );
 
-  shapeInit();
-
-  layerInit(&frettVfield );
-  layerDraw(&frettVfield );
-
-  drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
-
-  layerGetBounds(&frettVfield, &fieldFence);
+  layerInit(&notaR0 );
+  layerDraw(&notaR0 );
+  score = 0;
 
   enableWDTInterrupts();      /**< enable periodic interrupt */
   or_sr(0x8);	              /**< GIE (enable interrupts) */
 
   //Forever in C
   for(;;) { 
+      drawString5x7(80,40, score, COLOR_BLACK, bgColor);
+      u_int switches = p2sw_read(), i;
+      char str[5];
+      for (i = 0; i < 4; i++)
+          str[i] = (switches & (1<<i)) ? '-' : '0'+i;
+      str[4] = 0;
+      drawString5x7(80,20, str, COLOR_GREEN, bgColor);
+
       /** Watchdog will interrupt this and turn on the CPU */
-    while (!redrawScreen) { /**< Pause CPU if screen doesn't need updating */
-      P1OUT &= ~GREEN_LED;    /**< Green led off witHo CPU */
-      or_sr(0x10);	      /**< CPU OFF */
-    }
-    P1OUT |= GREEN_LED;       /**< Green led on when CPU on */
-    redrawScreen = 0;
-    movLayerDraw(&mlV, &notaV0);
-    //TEST SCORE
-    drawString5x7(100,200, "hello", COLOR_GREEN, COLOR_RED);
+      while (!redrawScreen) { /**< Pause CPU if screen doesn't need updating */
+          P1OUT &= ~GREEN_LED;    /**< Green led off witHo CPU */
+          or_sr(0x10);	      /**< CPU OFF */
+      }
+      P1OUT |= GREEN_LED;       /**< Green led on when CPU on */
+      redrawScreen = 0;
+      movLayerDraw(&mlV, &notaV0);
+      movLayerDraw(&mlR, &notaR0);
   }
 }
+
+
+void mlAdvance(MovLayer *ml, Layer *zone1, Layer *zone2, Layer *zone3, char carril){
+    //Vector2 for location
+    Vec2 newPos;
+
+    //Boundaries for "Objects"
+    Region noteBound;
+    Region z1Bound;
+    Region z2Bound;
+    Region z3Bound;
+    layerGetBounds(zone1, &z1Bound);
+    layerGetBounds(zone2, &z2Bound);
+    layerGetBounds(zone3, &z3Bound);
+
+    // Get switches
+    u_int switches = p2sw_read(), i;
+    char str[5];
+
+    //Infinite for loop
+    for (; ml; ml = ml->next) {
+        vec2Add(&newPos, &ml->layer->posNext, &ml->velocity);
+        //Get bounds for each new posigion
+        abShapeGetBounds(ml->layer->abShape, &newPos, &noteBound);
+
+        for (i = 0; i < 4; i++)
+            str[i] = (switches & (1<<i));
+        str[4] = 0;
+
+        //Hit first zone
+        if((noteBound.botRight.axes[1] > z1Bound.topLeft.axes[1]) && (str[0] == 0)
+                && (noteBound.botRight.axes[1] < z2Bound.topLeft.axes[1])
+                && (noteBound.botRight.axes[1] < z3Bound.topLeft.axes[1]) )
+        {
+            //Missing && PRESS BUTT -> GIVE 1 POINT
+            newPos = selectPosition(carril);
+        }
+        //Hit second zone CHANGE TO BUTT 1
+        if((noteBound.botRight.axes[1] > z2Bound.topLeft.axes[1]) && (str[1] == 0)
+                && (noteBound.botRight.axes[1] < z3Bound.topLeft.axes[1]) )
+        {
+            newPos = selectPosition(carril);
+        }
+        //Hit third zone
+        if((noteBound.botRight.axes[1] > z3Bound.topLeft.axes[1]) && (str[2] == 0) )
+        {
+            newPos = selectPosition(carril);
+        }
+
+        ml->layer->posNext = newPos;
+
+    } //end infinite for
+}//end mlAdvance
+
 
 /** Watchdog timer interrupt handler. 15 interrupts/sec */
 void wdt_c_handler()
@@ -170,7 +272,8 @@ void wdt_c_handler()
   P1OUT |= GREEN_LED;		      /**< Green LED on when cpu on */
   count ++;
   if (count == 15) {
-    mlVAdv(&mlV, &fieldFence);
+    mlAdvance(&mlV,&lFrettVerde0,&lFrettVerde1,&lFrettVerde2,0);
+    mlAdvance(&mlR,&lFrettRojo0,&lFrettRojo1,&lFrettRojo2,1);
     if (p2sw_read())
       redrawScreen = 1;
     count = 0;
